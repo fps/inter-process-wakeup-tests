e7e99f04107e8507c81f7b33f549e7f2a9405836
FPS <mista.tapas@gmx.net>
2024-05-16 15:02:10 +0200

    hmm

--- clock_gettime.c
+++ clock_gettime.c
@@ -1,10 +1,17 @@
 #include <time.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
-  struct timespec ts;
+  struct timespec ts1;
+  clock_gettime(CLOCK_MONOTONIC, &ts1);
+
+  struct timespec ts2;
   for (int index = 0; index < 1000000; ++index)
   {
-    clock_gettime(CLOCK_MONOTONIC, &ts);
+    clock_gettime(CLOCK_MONOTONIC, &ts2);
   }
+  
+  printf("secs: %ld, nsecs: %ld\n", ts1.tv_sec, ts1.tv_nsec);
+  printf("secs: %ld, nsecs: %ld\n", ts2.tv_sec, ts2.tv_nsec);
 }
